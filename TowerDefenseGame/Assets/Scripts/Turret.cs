05c341986adf29fc36acccb3c99d1c3e4f0a2c34
pablo <taddeiApablo23@gmail.com>
2022-09-09 18:54:47 -0300

    continuando
    continuando

--- TowerDefenseGame/Assets/Scripts/Turret.cs
+++ TowerDefenseGame/Assets/Scripts/Turret.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+
+    // Instance of the class
+    [Header("Turret Attributes")]
+    public static Turret instance;
+    public Transform Shootpoint;
+    public GameObject bullet;
+
+    [Header("Turret Properties")]
+    public int lives = 5;
+    public int CurrentLives = 5;
+    public float ViewDistance;
+    public float FireRate;
+    public float Force;
+    public float speed = 1.5f;
+    public bool isChildTurret = false;
+
+    private float nextTimeToFire = 0;
+    private Transform Target;
+    private Vector3 closeEnemyRef;
+    private Vector2 Direction;
+    private bool Detected = false;
+
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
